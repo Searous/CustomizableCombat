9bd5f6e4e7596374d6df04e7f0362c4facd9c422
Katie Kennedy <searousx20@gmail.com>
2021-12-10 14:36:03 -0600

    Fixed PAPI expansion not working

--- src/main/java/searous/customizableCombat/main/InternalPAPIExpansion.java
+++ src/main/java/searous/customizableCombat/main/InternalPAPIExpansion.java
@@ -4,7 +4,8 @@ import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
+
+import java.util.logging.Level;
 
 public class InternalPAPIExpansion extends PlaceholderExpansion {
     private final CustomizableCombat plugin;
@@ -15,27 +16,21 @@ public class InternalPAPIExpansion extends PlaceholderExpansion {
     
     // Set our placeholders
     @Override
-    public String onPlaceholderRequest(Player player, @NotNull String params) {
-        String output;
-        if(params.equalsIgnoreCase("customizablecombat_pvp_preference")) {
-            output = getOnOff(plugin.getPvpEnabled(player.getUniqueId()));
+    public String onRequest(OfflinePlayer player, String params) {
+        if(params.equalsIgnoreCase("pvp_preference")) {
+            String output = getOnOff(plugin.getPvpEnabled(player.getUniqueId()));
             return ChatColor.stripColor(output);
         }
         
-        return null;
-    }
+       if(params.equalsIgnoreCase("pvp_override_enabled")) {
+           return getOnOff(plugin.getPvpEnabledGlobal());
+       }
     
-    @Override
-    public String onRequest(OfflinePlayer player, @NotNull String params) {
-        if(params.equalsIgnoreCase("customizablecombat_pvp_override_enabled")) {
-            return getOnOff(plugin.getPvpEnabledGlobal());
-        }
-        
-        if(params.equalsIgnoreCase("customizablecombat_pvp_override_setting")) {
-            return getOnOff(plugin.getPvpEnabledOverride());
-        }
-        
-        return null;
+       if(params.equalsIgnoreCase("pvp_override_setting")) {
+           return getOnOff(plugin.getPvpEnabledOverride());
+       }
+    
+       return null;
     }
     
     //
@@ -46,17 +41,17 @@ public class InternalPAPIExpansion extends PlaceholderExpansion {
     
     // Required strings
     @Override
-    public @NotNull String getIdentifier() {
-        return "CustomizableCombat";
+    public String getIdentifier() {
+        return "customizablecombat";
     }
     
     @Override
-    public @NotNull String getAuthor() {
+    public String getAuthor() {
         return "Katie (Searous)";
     }
     
     @Override
-    public @NotNull String getVersion() {
+    public String getVersion() {
         return "v1.0";
     }
     
